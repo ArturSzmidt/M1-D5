88dc588ab7d2c2268014c5f7ad53f875fca4739d
ArturSzmidt <80460700+ArturSzmidt@users.noreply.github.com>
2021-03-12 10:13:09 +0100

    ++

--- d5.js
+++ d5.js
@@ -15,18 +15,28 @@
 /* Ex.A
    Create a variable called "test" and assign a string to it.
 */
+let test = "this is a test"
 
 /* Ex.B
     Create a variable called "sum" and assign to it the result of the sum between 10 and 20.
 */
+let sum = 10 + 20 
+console.log(sum)
 
 /* Ex.C 
     Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
+let random = (Math.floor(Math.random() * 10))
+console.log(random)
 
 /* Ex.D
-    Create a variable called "me" and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
+    Create a variable called "me" and assign to it an object containing the following information:
+     name = your name, surname = your surname, age = your age.
 */
+
+let me ={
+  
+}
 
 /* Ex.E 
     Programmatically remove the age property from the previously create object.
